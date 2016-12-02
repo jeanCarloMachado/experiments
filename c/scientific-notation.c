681c7b6f8f84b302771c0bb497070485220be183
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2016-12-02 21:37:52 -0200

    wip

--- c/scientific-notation.c
+++ c/scientific-notation.c
@@ -2,11 +2,11 @@
 
 int main() {
 
-    float var = 1.54334E-34;
-    double var2 = 1.54334E-34;
+    double var = 1.54334E-3;
 
-    printf("\n normal:%f\n sci:%e \n or \n sci:%E   \n",var,var,var);
-    printf("\n normal:%f\n sci:%e \n or \n sci:%E   \n",var2,var2* 1.0E3 ,var2 * 1.0e3);
+
+    double result  = var * 1.0e-03;
+    printf("%f sci:%e   \n", result, result);
     return 0;
 }
 
