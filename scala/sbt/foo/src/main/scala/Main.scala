1a527f148256e2306b703ed1cdc283eb278268ed
Jean Machado <contato@jeancarlomachado.com.br>
2018-05-26 13:32:39 +0200

    wip

---
+++ scala/sbt/foo/src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+import collection.mutable.Stack
+import org.scalatest._
+
+
+object Main extends FlatSpec {
+
+  "A stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    assert(stack.pop() === 2)
+    assert(stack.pop() === 1)
+  }
+}
---
+++ scala/scalaCli.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env scala
+import scala.io.StdIn
+
+var line = ""
+while ({line = StdIn.readLine(); line != null}) {
+    println(line)
+}
