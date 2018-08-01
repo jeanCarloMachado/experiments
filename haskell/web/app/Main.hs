030a32a0e577be07021298cc313ed77a9c71250b
jeanCarloMachado <contato@jeancarlomachado.com.br>
2018-08-01 14:00:27 +0200

    wip

--- haskell/web/app/Main.hs
+++ haskell/web/app/Main.hs
@@ -2,5 +2,23 @@
 import Web.Scotty
 
 main = scotty 3000 $ do
-  get "/" $ do
-    html "Hello World!"
+  get "/schduler" $ do
+    json [(0::Int)..10]
+  delete "/" $ do
+    text "deleted!"
+
+  post "/" $ do
+    text "posted!"
+
+  put "/" $ do
+    text "put-ted!"
+
+  matchAny "/all" $ do
+    text "matches all methods"
+
+  get "/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
+  notFound $ do
+    text "there is no such route."
