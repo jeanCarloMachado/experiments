360af8e614845cd966ca93a3de446745e8c8a23f
jeanCarloMachado <contato@jeancarlomachado.com.br>
2018-08-20 07:47:32 +0200

    automatic commit

--- scala/HelloWorld.scala
+++ scala/HelloWorld.scala
@@ -1,3 +1,4 @@
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     val foo: (Option[String], Option[String]) = (None,  Some("def"));
@@ -8,8 +9,6 @@ object HelloWorld {
   }
 
 
-  def getFoo(data:((Int, Int), (Int,Int)) ) : Int {
-      val ((a,b), (c,d)) = data
-      return a+b+c+d
+  def getFoo(data:((Int, Int), (Int,Int)) ) : Unit {
   }
 }
--- scala/StackSpect.scala
+++ scala/StackSpect.scala
@@ -5,7 +5,7 @@ class StackSpect extends FlatSpec {
 
   "A stack" should "pop values in last-in-first-out order" in {
     val stack = new Stack[Int]
-    stack.push(1)
+    stack.push(1
     stack.push(2)
     assert(stack.pop() === 2)
     assert(stack.pop() === 1)
