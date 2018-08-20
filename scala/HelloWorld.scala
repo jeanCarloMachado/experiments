95150dd37ef4a94164c79d5103dbe1f30e569929
jeanCarloMachado <contato@jeancarlomachado.com.br>
2018-08-20 07:09:17 +0200

    automatic commit

--- scala/HelloWorld.scala
+++ scala/HelloWorld.scala
@@ -1,6 +1,6 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    val foo: (Option[String], Option[String]) = (None,  Some("def"));
+    val foo (Option[String], Option[String]) = (None,  Some("def"));
 
     val data = ((1,2), (3,4))
     val result = getFoo(data)
